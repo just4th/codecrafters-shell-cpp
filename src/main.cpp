58aa5e8ddf469fa28dc2e138130dd21e6bd06fb3
Timur <timur2002164@gmail.com>
2026-06-25 15:46:54 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+#include <print>
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+
+  std::string s;
+  std::cin >> s;
+  std::println("{}: command not found", s);
 }
