7b3186705648f45a327acceafbe2a0fdaee8688c
Timur <timur2002164@gmail.com>
2026-06-25 16:52:48 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,40 +2,73 @@
 #include <sstream>
 #include <string>
 #include <print>
+#include <map>
+#include <functional>
+
+using CommandHandler = std::function<void(std::stringstream&&)>;
+
+void EchoHandler(std::stringstream&& stream);
+void TypeHandler(std::stringstream&& stream);
+
+const std::unordered_map<std::string, const CommandHandler> HANDLERS = {
+  {"echo", EchoHandler},
+  {"type", TypeHandler},
+  {"exit", {}}
+};
+
+void EchoHandler(std::stringstream&& stream) {
+  std::string arg;
+  if (!(stream >> arg)) {
+    return;
+  }
+
+  std::print("{}", arg);
+  while(stream >> arg) {
+    std::print(" {}", arg);
+  }
+}
+
+void TypeHandler(std::stringstream&& stream) {
+  std::string arg;
+  if (stream >> arg) {
+    if (HANDLERS.contains(arg)) {
+      std::print("{} is a shell builtin", arg);
+    } else {
+      std::print("{}: not found", arg);
+    }
+  }
+  return;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::string command;
-  std::string arg;
-
   for(;;) {
-    std::cout << "$ ";
+    std::print("$ ");
 
+    std::string command;
     std::getline(std::cin, command);
     std::stringstream stream(command);
 
-    if (!(stream >> arg)) {
+    std::string name;
+    if (!(stream >> name)) {
       continue;
     }
 
-    if (arg == "exit") {
+    if (name == "exit") {
       break;
     }
-    if (arg == "echo") {
-      if (stream >> arg) {
-        std::print("{}", arg);
-          while(stream >> arg) {
-            std::print(" {}", arg);
-          }
-      }
-      std::println();
+
+    auto it = HANDLERS.find(name);
+    if (it == HANDLERS.end()) {
+      std::println("{}: command not found", name);
       continue;
     }
 
-    std::println("{}: command not found", command);
+    it->second(std::move(stream));
+    std::println();
   }
 
   return 0;
