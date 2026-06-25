9921926f23a65e07e5e8653dff9ff0643ab72b2b
Timur <timur2002164@gmail.com>
2026-06-25 16:29:30 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <print>
 
@@ -7,17 +8,33 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-
-  
-
-
   std::string command;
+  std::string arg;
+
   for(;;) {
     std::cout << "$ ";
-    std::cin >> command;
-    if (command == "exit") {
+
+    std::getline(std::cin, command);
+    std::stringstream stream(command);
+
+    if (!(stream >> arg)) {
+      continue;
+    }
+
+    if (arg == "exit") {
       break;
     }
+    if (arg == "echo") {
+      if (stream >> arg) {
+        std::print("{}", arg);
+          while(stream >> arg) {
+            std::print(" {}", arg);
+          }
+      }
+      std::println();
+      continue;
+    }
+
     std::println("{}: command not found", command);
   }
 
